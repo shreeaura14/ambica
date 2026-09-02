b2d4937c7ce60ecc18e5fa4cb9672ec249f3dd4f
shreeaura14 <shreeaura.tas@gmail.com>
2026-09-02 23:16:31 +0530

    Create App.tsx

---
+++ src/app/App.tsx
@@ -0,0 +1,14 @@
+import { RouterProvider } from "react-router";
+import { router } from "./routes";
+import { AuthProvider } from "./contexts/AuthContext";
+import { CartProvider } from "./contexts/CartContext";
+
+export default function App() {
+  return (
+    <AuthProvider>
+      <CartProvider>
+        <RouterProvider router={router} />
+      </CartProvider>
+    </AuthProvider>
+  );
+}
